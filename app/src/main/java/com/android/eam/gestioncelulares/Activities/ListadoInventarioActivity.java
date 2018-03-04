dadb9b19148ef0c6a31f20b53edf93933f50e48c
Daryl110 <daryl.8220@gmail.com>
2018-03-04 14:53:28 -0500

    Interfaz de marca terminada

---
+++ app/src/main/java/com/android/eam/gestioncelulares/Activities/ListadoInventarioActivity.java
@@ -0,0 +1,15 @@
+package com.android.eam.gestioncelulares.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.android.eam.gestioncelulares.R;
+
+public class ListadoInventarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_inventario);
+    }
+}
